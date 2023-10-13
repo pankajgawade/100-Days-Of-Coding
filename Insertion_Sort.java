1df08e91992d0cbf873d1038fef78423c4221d4d
Pankaj Gawade <135791801+pankajgawade@users.noreply.github.com>
2023-10-13 23:27:27 +0530

    Add files via upload

---
+++ Insertion_Sort.java
@@ -0,0 +1,19 @@
+public class Insertion_Sort {
+    public static void main(String[] args) {
+        int[] arr = {4,8,5,1,2};
+        Insertion(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void Insertion(int []arr){
+        for(int i=0;i<arr.length-1;i++){
+            for(int j= i+1;j>0;j--){
+                if(arr[j]<arr[j-1]){
+                    swap(arr,j,j-1);
+                }
+                else{
+                    break;
+                }
+            }
+        }
+    } 
+}
